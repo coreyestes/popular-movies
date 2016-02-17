195ef696d1ea3c6fa7191156f8dd8492588ac47e
Corey Estes <cestes@honkforhelp.com>
2016-02-17 13:20:35 -0800

    Upload application to github

---
+++ app/src/main/java/com/rcdev/popularmovies/activity/FavoriteActivity.java
@@ -0,0 +1,123 @@
+package com.rcdev.popularmovies.activity;
+
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.rcdev.popularmovies.R;
+import com.rcdev.popularmovies.data.MovieContract;
+
+/**
+ * Created by coreyestes
+ * 2016 02 16
+ */
+public class FavoriteActivity extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private Cursor mDetailCursor;
+    private ImageView poster;
+    private ImageView backdrop;
+    private TextView movie_title;
+    private TextView movie_release;
+    private TextView movie_rating;
+    private TextView movie_overview;
+    private ListView trailer;
+    private ListView review;
+    private Cursor mCursor;
+    private int mPosition;
+
+    private Uri mUri;
+    private static final int CURSOR_LOADER_ID = 0;
+
+    public static FavoriteActivity newInstance(int position, Uri uri) {
+        FavoriteActivity fragment = new FavoriteActivity();
+        Bundle args = new Bundle();
+        fragment.mPosition = position;
+        fragment.mUri = uri;
+        args.putInt("id", position);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public FavoriteActivity() {
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String selection = null;
+        String [] selectionArgs = null;
+        if (args != null){
+            selection = MovieContract.FavoriteEntry._ID;
+            selectionArgs = new String[]{String.valueOf(mPosition)};
+        }
+        return new CursorLoader(getActivity(),
+                mUri,
+                null,
+                selection,
+                selectionArgs,
+                null);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        poster = (ImageView) rootView.findViewById(R.id.ivPoster);
+        backdrop= (ImageView) rootView.findViewById(R.id.ivBackdrop);
+        movie_title = (TextView)rootView.findViewById(R.id.tvTitle);
+        movie_release = (TextView) rootView.findViewById(R.id.tvReleaseDate);
+        movie_rating = (TextView)rootView.findViewById(R.id.tvRating);
+        movie_overview = (TextView) rootView.findViewById(R.id.tvOverview);
+        trailer = (ListView)rootView.findViewById(R.id.lvTrailer);
+        review = (ListView) rootView.findViewById(R.id.lvReviews);
+
+        Bundle args = this.getArguments();
+        getLoaderManager().initLoader(CURSOR_LOADER_ID, args, FavoriteActivity.this);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+//        mListener = null;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    // Set the cursor in our CursorAdapter once the Cursor is loaded
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursor = data;
+        mCursor.moveToFirst();
+        DatabaseUtils.dumpCursor(data);
+        poster.setImageResource(mDetailCursor.getInt(5));
+        backdrop.setImageResource(mDetailCursor.getInt(4));
+        movie_title.setText(mDetailCursor.getString(2));
+        movie_release.setText(mDetailCursor.getString(8));
+        movie_rating.setText(mDetailCursor.getString(7));
+        movie_overview.setText(mDetailCursor.getString(6));
+
+    }
+
+    // reset CursorAdapter on Loader Reset
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader){
+        mCursor = null;
+    }
+}
---
+++ app/src/main/java/com/rcdev/popularmovies/objects/MovieItem.java
@@ -0,0 +1,216 @@
+package com.rcdev.popularmovies.objects;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.rcdev.popularmovies.utils.Constants;
+
+/**
+ * Created by coreyestes on 7/22/15.
+ */
+public class MovieItem implements Parcelable {
+    private String thumb;
+    private String id;
+    private String title;
+    private String poster;
+    private String back_drop;
+    private String overview;
+    private String release_date;
+    private String rating;
+    private String results;
+    private String path;
+    private String popularity;
+    private Double vote_average;
+    private String vote_count;
+    private String full_poster;
+    private int image;
+
+    public MovieItem() {
+        super();
+    }
+
+    protected MovieItem(Parcel in) {
+        id = in.readString();
+        thumb = in.readString();
+        results = in.readString();
+        title = in.readString();
+        path = in.readString();
+        full_poster = in.readString();
+        back_drop = in.readString();
+        overview = in.readString();
+        release_date = in.readString();
+        popularity = in.readString();
+        vote_count = in.readString();
+        image = in.readInt();
+
+
+    }
+
+    public static final Creator<MovieItem> CREATOR = new Creator<MovieItem>() {
+        @Override
+        public MovieItem createFromParcel(Parcel in) {
+            return new MovieItem(in);
+        }
+
+        @Override
+        public MovieItem[] newArray(int size) {
+            return new MovieItem[size];
+        }
+    };
+
+    public MovieItem(String id, String thumb, String title, String full_poster, String backdrop, String overview, String release_date, String rating) {
+
+    }
+
+    public String getBackdrop() {
+        return back_drop;
+    }
+
+    public void setBackdrop(String backdrop) {
+        this.back_drop = Constants.BASE_IMAGE_PATH + backdrop;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public String getFull_poster() {
+        return full_poster;
+    }
+
+    public void setFull_poster(String full_poster) {
+        this.full_poster = Constants.BASE_IMAGE_PATH + path;
+    }
+
+    public String getResults() {
+        return results;
+    }
+
+    public void setResults(String results) {
+        this.results = results;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public Double getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(Double vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public String getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(String vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getBack_drop() {
+        return back_drop;
+    }
+
+    public void setBack_drop(String back_drop) {
+        this.back_drop = back_drop;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(results);
+        dest.writeString(thumb);
+        dest.writeString(path);
+        dest.writeString(title);
+        dest.writeString(back_drop);
+        dest.writeString(overview);
+        dest.writeString(release_date);
+        dest.writeString(popularity);
+        dest.writeString(vote_count);
+        dest.writeString(full_poster);
+        dest.writeInt(image);
+    }
+}
+
+
+
